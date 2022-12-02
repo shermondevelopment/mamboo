e46d461d427fa7a77bf733f616d1274616f13149
shermondevelopment <victorshermon@yahoo.com>
2022-12-01 21:57:12 -0300

    feat: connecting to the database before starting the server

--- app/server.ts
+++ app/server.ts
@@ -4,7 +4,16 @@ import 'dotenv/config'
 /** express */
 import express from 'express'
 
+/** connection how db */
+import connectionDb from './settings/db'
+
 /** server */
 const app = express()
 
-app.listen(process.env.PORT_APP, () => console.log('app running in port 3001'))
+connectionDb
+  .then(() => {
+    app.listen(process.env.PORT_APP, () =>
+      console.log('app running in port 3001')
+    )
+  })
+  .catch(() => console.log('connection with database failed ❌❌❌'))
